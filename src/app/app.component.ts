baf4ecccdf58b820869ebe35e28e5fec26ce5687
Luiz Negrinho <luizavm@gmail.com>
2021-05-10 13:58:44 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularDH';
+  photos= [
+     {
+       description: 'Digital House',
+       url: 'https://www.digitalhouse.com/br/assets/metas/200903170927-2p33kws0ken8tfwd.png'
+     },
+     {
+      description: 'Digital House',
+      url: 'https://www.digitalhouse.com/br/assets/metas/200903170927-2p33kws0ken8tfwd.png'
+    },
+    {
+      description: 'Digital House',
+      url: 'https://www.digitalhouse.com/br/assets/metas/200903170927-2p33kws0ken8tfwd.png'
+    },
+    {
+      description: 'Digital House',
+      url: 'https://www.digitalhouse.com/br/assets/metas/200903170927-2p33kws0ken8tfwd.png'
+    }
+  ]
 }
+
